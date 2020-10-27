e2ea5b76d670cc4be040c14347d4b73c00677b33
femastro <femastro@gmail.com>
2020-10-27 00:42:41 -0300

    --amend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContainerComponent } from './shared/components/container/container.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path : '',
+    component : ContainerComponent,
+    children : [
+
+      {
+        path : '',
+        redirectTo : 'home',
+        pathMatch : 'full'
+      },
+      { 
+        path: 'home', 
+        loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) 
+      },
+      { 
+        path: 'productos', 
+        loadChildren: () => import('./pages/productos/productos.module').then(m => m.ProductosModule) 
+      },
+
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './shared/components/container/container.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
